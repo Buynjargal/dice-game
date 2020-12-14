e575388a5e39059f0c2a448e8f7a51de9bbf5ce3
Buyanjargal <oexebuyanjargal8a@gmail.com>
2020-12-14 11:34:53 +0800

    Bra, Togloomoo hiij duuslaa.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // Togloomiin buh gazar ashiglagdah buh huvisagchdiig end zarlay
+//Togloom duussan esehiig hadgalah toloviin huvisagch
+var isNewGame;
 // henii eelj ve gedgiig hadgalna.
 var activePlayer;
 //2 toglogchiin tsugluulsan onoonuud bna
@@ -14,6 +16,9 @@ initGame();
 
 //Togloomiig shineer ehlehed beltgene.
 function initGame(){
+    //Togloom ehellee gedeg tolovt oruulna.
+    isNewGame = true;
+
     // Toglogchiin eeljiig hadgalah huvisagch, 1dugeer toglogchiig 0, 2 dugaar toglogchiig 1 gey.
     activePlayer = 0;
 
@@ -50,36 +55,46 @@ function initGame(){
 
 //Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function(){
-    //1-6 dotorh sanamsargui neg too gargaj avna
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-    //shoonii zurgiig web der gargaj irne.
-    diceDom.style.display = "block";
+    if(isNewGame){
+        //1-6 dotorh sanamsargui neg too gargaj avna
+        var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-    //buusan toond hargalzah shoonii zurgiig web der gargaj irne.
-    diceDom.src = "dice-" + diceNumber + ".png";
+        //shoonii zurgiig web der gargaj irne.
+        diceDom.style.display = "block";
 
-    //Buusan too 1 ees yalgaatai bol idevhtei toglogchiin eeljiin onoog nemegduulne.
-    if(diceNumber !== 1){
-        // 1ees yalgaatai too buulaa. Buusan toog toglogchid nemj ogno.
-        roundScore = roundScore + diceNumber;
-        document.getElementById("current-" + activePlayer).textContent = roundScore;
-    }else{
-        //1 buusan bol toglogchiin eeljjig end solij ogno.
-        switchToNextPlayer();
+        //buusan toond hargalzah shoonii zurgiig web der gargaj irne.
+        diceDom.src = "dice-" + diceNumber + ".png";
+
+        //Buusan too 1 ees yalgaatai bol idevhtei toglogchiin eeljiin onoog nemegduulne.
+        if(diceNumber !== 1){
+            // 1ees yalgaatai too buulaa. Buusan toog toglogchid nemj ogno.
+            roundScore = roundScore + diceNumber;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+        }else{
+            //1 buusan bol toglogchiin eeljjig end solij ogno.
+            switchToNextPlayer();
+        }
+    } else {
+        alert("Togloom duussan bna.New game iig darj shineer ehlenuu.");
     }
+
 });
 
 //HOLD tovchnii event listener
 document.querySelector(".btn-hold").addEventListener("click", function(){
-    //ug toglogchiin tsugluulsan onoog global onoon der ni nemne.
-    scores[activePlayer] = scores[activePlayer] + roundScore;
+    if(isNewGame){
+        //ug toglogchiin tsugluulsan onoog global onoon der ni nemne.
+        scores[activePlayer] = scores[activePlayer] + roundScore;
 
-    //Delgets der onoog ni oorchilno.
-    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+        //Delgets der onoog ni oorchilno.
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
-    //Ug toglogch hojson esehiig shalgah. onoo ni 100gaas ih eseh.
-    if(scores[activePlayer] >= 20){
+        //Ug toglogch hojson esehiig shalgah. onoo ni 100gaas ih eseh.
+        if(scores[activePlayer] >= 20){
+        //Togloomiig duussan tolovt oruulna
+        isNewGame = false;
+
         //Yalagch gesen text iig nerniih ni orond gargana
         document.getElementById("name-" + activePlayer).textContent = "WINNER!!!"; 
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
@@ -88,6 +103,9 @@ document.querySelector(".btn-hold").addEventListener("click", function(){
     } else {
         //Toglogchiin eeljiig solino
         switchToNextPlayer();
+        }
+    }else{
+        alert("Togloom duussan bna.New game iig darj shineer ehlenuu. hold");
     }
 
 });
@@ -111,3 +129,4 @@ function switchToNextPlayer(){
 
 // NEW GAME buyu Shine togloom ehluuleh event listener
 document.querySelector(".btn-new").addEventListener("click", initGame);
+
